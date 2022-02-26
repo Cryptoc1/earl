63174dd80b535de763dedc9b529af80aa6a4b730
Samuel Steele <steelesam72@gmail.com>
2022-02-26 16:07:18 -0500

    support all CrawlerOptions properties via fluent ICrawlerOptionsBuilder

--- src/Crawler/Crawler/Configuration/CrawlerOptionsBuilderExtensions.cs
+++ src/Crawler/Crawler/Configuration/CrawlerOptionsBuilderExtensions.cs
@@ -1,16 +1,35 @@
-﻿namespace Earl.Crawler.Configuration;
+﻿using Earl.Crawler.Abstractions;
+
+namespace Earl.Crawler.Configuration;
 
 /// <summary> Extensions to <see cref="ICrawlerOptionsBuilder"/>. </summary>
 public static class CrawlerOptionsBuilderExtensions
 {
+    /// <summary> Configure the <see cref="CrawlerOptions.BatchDelay"/>. </summary>
+    /// <param name="builder"> The builder to configure. </param>
+    /// <param name="delay"> The desired batch delay. </param>
+    public static ICrawlerOptionsBuilder BatchDelay( this ICrawlerOptionsBuilder builder, TimeSpan delay )
+    {
+        ArgumentNullException.ThrowIfNull( builder );
+
+        builder.Properties[ nameof( CrawlerOptions.BatchDelay ) ] = delay;
+        return CrawlerOptionsBuilder.Decorate(
+            builder,
+            ( builder, options ) => options with { BatchDelay = builder.GetProperty<TimeSpan>( nameof( CrawlerOptions.BatchDelay ) ) }
+        );
+    }
+
+    /// <summary> Configure the <see cref="CrawlerOptions.BatchSize"/>. </summary>
+    /// <param name="builder"> The builder to configure. </param>
+    /// <param name="size"> The desired batch size. </param>
     public static ICrawlerOptionsBuilder BatchSize( this ICrawlerOptionsBuilder builder, int size )
     {
         ArgumentNullException.ThrowIfNull( builder );
 
-        builder.Properties[ nameof( BatchSize ) ] = size;
+        builder.Properties[ nameof( CrawlerOptions.BatchSize ) ] = size;
         return CrawlerOptionsBuilder.Decorate(
             builder,
-            ( builder, options ) => options with { BatchSize = builder.GetProperty<int>( nameof( BatchSize ) ) }
+            ( builder, options ) => options with { BatchSize = builder.GetProperty<int>( nameof( CrawlerOptions.BatchSize ) ) }
         );
     }
 
@@ -49,14 +68,45 @@ public static class CrawlerOptionsBuilderExtensions
             : default;
     }
 
+    /// <summary> Configure the <see cref="CrawlerOptions.MaxDegreeOfParallelism"/>. </summary>
+    /// <param name="builder"> The builder to configure. </param>
+    /// <param name="max"> The desired maximum degree of parallelism. </param>
+    public static ICrawlerOptionsBuilder MaxDegreeOfParallelism( this ICrawlerOptionsBuilder builder, int max )
+    {
+        ArgumentNullException.ThrowIfNull( builder );
+
+        builder.Properties[ nameof( CrawlerOptions.MaxDegreeOfParallelism ) ] = max;
+        return CrawlerOptionsBuilder.Decorate(
+            builder,
+            ( builder, options ) => options with { MaxDegreeOfParallelism = builder.GetProperty<int>( nameof( CrawlerOptions.MaxDegreeOfParallelism ) ) }
+        );
+    }
+
+    /// <summary> Configure the <see cref="CrawlerOptions.MaxRequestCount"/>. </summary>
+    /// <param name="builder"> The builder to configure. </param>
+    /// <param name="max"> The desired maximum request count. </param>
+    public static ICrawlerOptionsBuilder MaxRequestCount( this ICrawlerOptionsBuilder builder, int max )
+    {
+        ArgumentNullException.ThrowIfNull( builder );
+
+        builder.Properties[ nameof( CrawlerOptions.MaxRequestCount ) ] = max;
+        return CrawlerOptionsBuilder.Decorate(
+            builder,
+            ( builder, options ) => options with { MaxRequestCount = builder.GetProperty<int>( nameof( CrawlerOptions.MaxRequestCount ) ) }
+        );
+    }
+
+    /// <summary> Configure the <see cref="CrawlerOptions.Timeout"/>. </summary>
+    /// <param name="builder"> The builder to configure. </param>
+    /// <param name="timeout"> The desired timeout. </param>
     public static ICrawlerOptionsBuilder Timeout( this ICrawlerOptionsBuilder builder, TimeSpan timeout )
     {
         ArgumentNullException.ThrowIfNull( builder );
 
-        builder.Properties[ nameof( Timeout ) ] = timeout;
+        builder.Properties[ nameof( CrawlerOptions.Timeout ) ] = timeout;
         return CrawlerOptionsBuilder.Decorate(
             builder,
-            ( builder, options ) => options with { Timeout = builder.GetProperty<TimeSpan?>( nameof( Timeout ) ) }
+            ( builder, options ) => options with { Timeout = builder.GetProperty<TimeSpan?>( nameof( CrawlerOptions.Timeout ) ) }
         );
     }
 }
