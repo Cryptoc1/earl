29637b10d52f143b3dff226a9c687f55f6826abf
Samuel Steele <steelesam72@gmail.com>
2022-02-27 15:59:19 -0500

    update cli spinner status messages

--- src/Agent/Console/DefaultCommand.cs
+++ src/Agent/Console/DefaultCommand.cs
@@ -26,24 +26,25 @@ public class DefaultCommand : CancellableAsyncCommand
                 $"Initiating crawl",
                 async context =>
                 {
-                    await Task.Delay( 500 );
+                    await Task.Delay( 1000 );
 
                     var url = new Uri( "https://webscraper.io/test-sites/e-commerce/static" );
                     var options = CrawlerOptionsBuilder.CreateDefault()
                         .On<CrawlErrorEvent>( onError )
                         .On<CrawlUrlResultEvent>( onUrlResult )
+                        .On<CrawlUrlStartedEvent>( onUrlStarted )
                         .Build();
 
                     var crawl = crawler.CrawlAsync( url, options, cancellation );
 
                     context.SpinnerStyle( Style.Parse( "green bold" ) );
                     context.Spinner( Spinner.Known.Triangle );
-                    context.Status( $"Crawling" );
+                    context.Status( $"Crawling '{url}'" );
 
                     await Task.Delay( 1500 );
                     await crawl;
 
-                    static Task onError( CrawlErrorEvent e, CancellationToken cancellation )
+                    Task onError( CrawlErrorEvent e, CancellationToken cancellation )
                     {
                         if( e.Url is not null )
                         {
@@ -53,13 +54,19 @@ public class DefaultCommand : CancellableAsyncCommand
                         return Task.CompletedTask;
                     }
 
-                    static Task onUrlResult( CrawlUrlResultEvent e, CancellationToken cancellation )
+                    Task onUrlStarted( CrawlUrlStartedEvent e, CancellationToken cancellation )
+                    {
+                        context.Status( $"Crawling '{e.Url}'" );
+                        return Task.CompletedTask;
+                    }
+
+                    Task onUrlResult( CrawlUrlResultEvent e, CancellationToken cancellation )
                     {
                         AnsiConsole.MarkupLine( $"[green]✔[/] {e.Result.Url}" );
                         return Task.CompletedTask;
                     }
                 }
-            );
+           );
 
         return 0;
     }
