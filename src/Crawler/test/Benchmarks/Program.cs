83f9445f5aec475d4117987b6edeebf4116359cd
Samuel Steele <steelesam72@gmail.com>
2022-03-23 16:55:22 -0400

    enable inlining & native memory diagnosers

--- src/Crawler/test/Benchmarks/Program.cs
+++ src/Crawler/test/Benchmarks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Jobs;
@@ -12,38 +13,35 @@ using BenchmarkDotNet.Toolchains.DotNetCli;
 var config = DefaultConfig.Instance
     .AddColumn( StatisticColumn.Max )
     .AddColumn( StatisticColumn.Min )
+    .AddDiagnoser( new InliningDiagnoser() )
     .AddDiagnoser( MemoryDiagnoser.Default )
+    .AddDiagnoser( new NativeMemoryProfiler() )
     .AddDiagnoser( ThreadingDiagnoser.Default )
     .AddExporter( MarkdownExporter.GitHub )
     .AddExporter( DefaultExporters.JsonFullCompressed )
     .AddJob(
-        Job.ShortRun
-            .AsBaseline()
+        Job.ShortRun.AsBaseline()
             .WithId( "default" )
             .WithJit( Jit.RyuJit )
             .WithPlatform( Platform.AnyCpu )
     )
     .AddJob(
-        Job.RyuJitX64
-            .UnfreezeCopy()
+        Job.RyuJitX64.UnfreezeCopy()
             .ApplyAndFreeze( Job.ShortRun )
             .WithId( "x64" )
             .WithToolchain(
                 CsProjCoreToolchain.From(
-                    NetCoreAppSettings.NetCoreApp60
-                          .WithCustomDotNetCliPath( @"C:\Program Files\dotnet\dotnet.exe", ".NET (x64)" )
+                    NetCoreAppSettings.NetCoreApp60.WithCustomDotNetCliPath( @"C:\Program Files\dotnet\dotnet.exe", ".NET (x64)" )
                 )
             )
     )
     .AddJob(
-        Job.RyuJitX86
-            .UnfreezeCopy()
+        Job.RyuJitX86.UnfreezeCopy()
             .ApplyAndFreeze( Job.ShortRun )
             .WithId( "x86" )
             .WithToolchain(
                 CsProjCoreToolchain.From(
-                    NetCoreAppSettings.NetCoreApp60
-                          .WithCustomDotNetCliPath( @"C:\Program Files (x86)\dotnet\dotnet.exe", ".NET (x86)" )
+                    NetCoreAppSettings.NetCoreApp60.WithCustomDotNetCliPath( @"C:\Program Files (x86)\dotnet\dotnet.exe", ".NET (x86)" )
                 )
             )
     )
