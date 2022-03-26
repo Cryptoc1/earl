a692318d98ef57d79cc0be052db83d1fe50a1fd1
Samuel Steele <steelesam72@gmail.com>
2022-03-26 02:36:33 -0400

    more persistence tests

---
+++ src/Crawler/Persistence/Abstractions/test/Configuration/CrawlerPersistenceOptionsBuilderExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Earl.Crawler.Persistence.Abstractions.Configuration.Tests;
+
+public sealed class CrawlerPersistenceOptionsBuilderExtensionsTests
+{
+    [Fact]
+    public void Configure_adds_build_action_to_builder( )
+    {
+        static CrawlerPersistenceOptions action( ICrawlerPersistenceOptionsBuilder builder, CrawlerPersistenceOptions options ) => options;
+        var builder = new OptionsBuilder()
+            .Configure( action );
+
+        Assert.Collection( builder.BuildActions, a => Assert.Equal( action, a ) );
+    }
+
+    private sealed class OptionsBuilder : ICrawlerPersistenceOptionsBuilder
+    {
+        public IList<CrawlerPersistenceOptionsBuildAction> BuildActions { get; } = new List<CrawlerPersistenceOptionsBuildAction>();
+
+        public CrawlerPersistenceOptions Build( )
+        {
+            var options = new CrawlerPersistenceOptions( default! );
+            foreach( var action in BuildActions )
+            {
+                options = action( this, options );
+            }
+
+            return options;
+        }
+    }
+}
---
+++ src/Crawler/Persistence/Json/test/JsonCrawlerPersistenceBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Earl.Crawler.Persistence.Configuration;
+
+namespace Earl.Crawler.Persistence.Json.Tests;
+
+public sealed class CrawlerPersistenceBuilderJsonExtensions
+{
+    [Fact]
+    public void Json_persistence_registers_json_persistence_descriptor( )
+    {
+        var options = new CrawlerPersistenceOptionsBuilder()
+            .ToJson( _ => _.Destination( "." ) )
+            .Build();
+
+        Assert.Collection(
+            options.Descriptors,
+            descriptor => Assert.True( descriptor is CrawlerJsonPersistenceDescriptor )
+        );
+    }
+}
